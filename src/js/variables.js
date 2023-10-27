a05ba99d11e33fa1e3b58cd3313959a8fd1df4d9
Tounka <luisydiego201516@gmail.com>
2023-10-26 23:35:42 -0700

    logica agregada

---
+++ src/js/variables.js
@@ -0,0 +1,7 @@
+let inpTarifa = document.getElementById("tarifa");
+let inpTotalLuz = document.getElementById("totalLuz");
+let inpDisponibilidadSolar = document.getElementById("disponibilidadSolar");
+let inpTotalLuzEfectivo = document.getElementById("totalLuzEfectivo");
+let respuesta = document.getElementById("respuesta");
+
+let btnCalcular = document.getElementById("btnCalcular");
